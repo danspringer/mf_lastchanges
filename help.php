5f90b2eda61079c0491b5e72e47d5a79ab040e0a
medienfeuer <dsp.springer@googlemail.com>
2018-01-07 14:12:49 +0100

    Delete help.php

--- help.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-?>
-<b>Letzte Änderungen</b>
-
-<br /><br />
-Zeigt an, welche Artikel wann und von welchem Redaxo-Benutzer zuletzt geändert worden sind.<br />
-Bisher nur für einsprachige Seiten geeignet.
