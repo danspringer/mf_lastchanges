5e0f7acadf237decb0c1b9e1712b3da7801c4f6b
medienfeuer <dsp.springer@googlemail.com>
2018-01-07 14:06:25 +0100

    Add files via upload

---
+++ help.php
@@ -0,0 +1,8 @@
+<?php
+
+?>
+<b>Letzte Änderungen</b>
+
+<br /><br />
+Zeigt an, welche Artikel wann und von welchem Redaxo-Benutzer zuletzt geändert worden sind.<br />
+Bisher nur für einsprachige Seiten geeignet.
